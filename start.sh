6db3e2fb642201652542b4cf7301e0bf465cebb2
hllj <vanhop3499@gmail.com>
2024-03-14 05:14:35 +0000

    update new version vllm, try using AWQ + Marlin

--- start.sh
+++ start.sh
@@ -8,10 +8,11 @@
 # --dtype bfloat16 \
 
 python services/vllm/api_server.py \
---host 0.0.0.0 \
---port 8000 \
---disable-log-requests \
---model Qwen-7B-Chat-AWQ-Marlin \
---quantization marlin \
---trust-remote-code \
---dtype bfloat16 \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --disable-log-requests \
+    --model /root/AutoAWQ/mistral-instruct-v0.2-awq-marlin \
+    --quantization awq \
+    --enforce-eager \
+    --trust-remote-code \
+    --dtype float16 \
